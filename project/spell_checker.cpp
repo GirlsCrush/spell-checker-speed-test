cdfabb877b72d97d65c8357cbd220f24ae2b512f
U-DESKTOP-4JDMFGL\lionn <lionnnn351_at_gmail.com>
2019-05-23 15:17:25 +0300

    Trying to optimize

--- project/spell_checker.cpp
+++ project/spell_checker.cpp
@@ -3,9 +3,11 @@
 #include <set>
 #include <memory>
 #include <unordered_set>
+#include <unordered_map>
 #include <fstream>
 #include <algorithm>
 #include <functional>
+#include <thread>
 
 const unsigned int MAX_HASH = 501037;
 
@@ -17,6 +19,7 @@ unsigned int string_hash(const std::string &word, unsigned int max) {
 inline int getIndex(char c) {
 	return (c - 'a' >= 0 ? c - 'a' : 26);
 }
+
 class SpellChecker_Vector : public SpellChecker_Impl
 {
 public:
@@ -186,14 +189,24 @@ class TrieNode {
 public:
 	TrieNode() = default;
 	bool end = false;
-	TrieNode * next[27] = { nullptr, nullptr, nullptr, nullptr, nullptr,
-	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr };
-	~TrieNode() {
-		for (int i = 0; i < 27; ++i)
-			delete next[i];
-	}
+	TrieNode* next[123] = { nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr};
+	// ~TrieNode() {
+	// 	for (int i = 0; i < 91; ++i)
+	// 		if (!next[i])
+	// 			delete next[i];
+	// }
 };
 
 
@@ -203,32 +216,25 @@ private:
 	inline void push(const std::string &word) {
 		TrieNode * tmp = root;
 		std::string::const_iterator it = word.begin();
-		unsigned short index;
 		for (; it != word.end(); ++it) {
-			index = getIndex(*it);
-			if (!tmp->next[index]) {
-				break;
+				if (!tmp->next[*it]) {
+					break;
+				}
+				tmp = tmp->next[*it];
+		}
+		
+		if (it == word.end()) {
+			if (!tmp->end) {
+				tmp->end = true;
+				++_size;
 			}
-			tmp = tmp->next[index];
-		}
-		// if (it == word.end()) {
-		//     ++_size;
-		//     return;
-		// }
-		if (it != word.end())
-			tmp = tmp->next[getIndex(*it)] = (new TrieNode);
-		else if (!tmp->end) {
-			tmp->end = true;
-			++_size;
-			return;
-		}
-		else {
 			return;
 		}
 
-		while (++it != word.end()) {
-			tmp = tmp->next[getIndex(*it)] = (new TrieNode);
-		}
+		do {
+			tmp = tmp->next[(*it) & ~0x20]  = tmp->next[*it] = (new TrieNode);
+		} while (++it != word.end());
+
 		tmp->end = true;
 		++_size;
 	}
@@ -237,69 +243,47 @@ public:
 		std::ifstream file(dictionary);
 		if (file.fail())
 			throw SpellChecker_InvalidDictFile();
-		std::string word;
 
+		std::string word;
 		std::string::const_iterator it;
-		unsigned short index;
 		TrieNode * tmp;
-		while (file >> word) {
-			tmp = root;
-			it = word.begin();
-			for (; it != word.end(); ++it) {
-				index = getIndex(*it);
-				if (!tmp->next[index]) {
+		while (file >> word) {	//push(tmp);
+			
+			for (tmp = root, it = word.begin(); it != word.end(); ++it) {
+				if (!tmp->next[*it]) {
 					break;
 				}
-				tmp = tmp->next[index];
-			}
-			// if (it == word.end()) {
-			//     ++_size;
-			//     return;
-			// }
-			if (it != word.end())
-				tmp = tmp->next[getIndex(*it)] = (new TrieNode);
-			else if (!tmp->end) {
-				tmp->end = true;
-				++_size;
-				continue;
-			}
-			else {
-				continue;
+				tmp = tmp->next[*it];
 			}
 
-			while (++it != word.end()) {
-				tmp = tmp->next[getIndex(*it)] = (new TrieNode);
-			}
+			// Not needed here as words in dictionary are sorted
+
+			// if (it == word.end() && !tmp->end) {
+			// 	tmp->end = true;
+			// 	++_size;
+			// 	continue;
+			// }
+			// else {
+			// 	continue;
+			// }
+
+			do {
+				tmp = tmp->next[(*it) & ~0x20] = tmp->next[*it] = (new TrieNode);	// "& ~0x20" converts a char to upper case 
+			} while (++it != word.end());
 			tmp->end = true;
 			++_size;
 		}
-		//push(tmp);
 	}
 	inline bool check(const std::string &word) const {
-		//std::string wordLower(word);
-		//std::transform(word.begin(), word.end(), wordLower.begin(), ::tolower);
-		// for (int i = 0; i < wordLower.size(); ++i)
-		//     if (wordLower[i] !islower(wordLower[i]))
-		//         wordLower[i] = tolower(wordLower[i]);
-		// tolowerStr(wordLower);
-		//for ( auto it = wordLower.begin(); it != wordLower.end(); ++it )
-		//    *it |= 0x20;
-
 		TrieNode * tmp = root;
-		// for (char c : word) {
-		// 	tmp = tmp->next[getIndex(c | 0x20)];
-		// 	if (!tmp) {
-		// 		return false;
-		// 	}
-		// }
 		for (int i = 0; i < word.size(); ++i) {
-			tmp = tmp->next[getIndex(word[i] | 0x20)];
-			if (!tmp) {
+			if (!(tmp = tmp->next[word[i]])) {
 				return false;
 			}
 		}
 		return tmp->end;
 	}
+	
 	inline void add(const std::string &word) {
 		std::string wordLower(word);
 		std::transform(word.begin(), word.end(), wordLower.begin(), ::tolower);
@@ -307,11 +291,134 @@ public:
 	}
 	inline size_t size(void) const { return _size; }
 	SpellChecker_Trie() : root(new TrieNode()) {}
-	~SpellChecker_Trie() { delete root; }
+	//~SpellChecker_Trie() { delete root; }
 private:
 	TrieNode * root;
 	size_t _size = 0;
 };
+
+
+// class TrieNode {
+// public:
+// 	TrieNode() = default;
+// 	bool end = false;
+// 	std::unordered_map<char, TrieNode *> next;
+// };
+
+// class SpellChecker_Trie : public SpellChecker_Impl
+// {
+// private:
+// 	inline void push(const std::string &word) {
+// 		TrieNode * tmp = root;
+// 		std::string::const_iterator it = word.begin();
+// 		unsigned short index;
+// 		for (; it != word.end(); ++it) {
+// 			index = getIndex(*it);
+// 			if (!tmp->next[index]) {
+// 				break;
+// 			}
+// 			tmp = tmp->next[index];
+// 		}
+// 		// if (it == word.end()) {
+// 		//     ++_size;
+// 		//     return;
+// 		// }
+// 		if (it != word.end())
+// 			tmp = tmp->next[getIndex(*it)] = (new TrieNode);
+// 		else if (!tmp->end) {
+// 			tmp->end = true;
+// 			++_size;
+// 			return;
+// 		}
+// 		else {
+// 			return;
+// 		}
+
+// 		while (++it != word.end()) {
+// 			tmp = tmp->next.insert(*it);
+// 		}
+// 		tmp->end = true;
+// 		++_size;
+// 	}
+// public:
+// 	inline void load(const std::string &dictionary) {
+// 		std::ifstream file(dictionary);
+// 		if (file.fail())
+// 			throw SpellChecker_InvalidDictFile();
+
+// 		std::string word;
+// 		std::string::const_iterator it;
+// 		unsigned short index;
+// 		TrieNode * tmp;
+// 		while (file >> word) {	//push(tmp);
+// 			tmp = root;
+// 			it = word.begin();
+// 			for (; it != word.end(); ++it) {
+// 				index = getIndex(*it);
+// 				if (!tmp->next[index]) {
+// 					break;
+// 				}
+// 				tmp = tmp->next[index];
+// 			}
+// 			// if (it == word.end()) {
+// 			//     ++_size;
+// 			//     return;
+// 			// }
+// 			if (it != word.end())
+// 				tmp = tmp->next[getIndex(*it)] = (new TrieNode);
+// 			else if (!tmp->end) {
+// 				tmp->end = true;
+// 				++_size;
+// 				continue;
+// 			}
+// 			else {
+// 				continue;
+// 			}
+
+// 			while (++it != word.end()) {
+// 				tmp = tmp->next[getIndex(*it)] = (new TrieNode);
+// 			}
+// 			tmp->end = true;
+// 			++_size;
+// 		}
+// 	}
+// 	inline bool check(const std::string &word) const {
+// 		//std::string wordLower(word);
+// 		//std::transform(word.begin(), word.end(), wordLower.begin(), ::tolower);
+// 		// for (int i = 0; i < wordLower.size(); ++i)
+// 		//     if (wordLower[i] !islower(wordLower[i]))
+// 		//         wordLower[i] = tolower(wordLower[i]);
+// 		// tolowerStr(wordLower);
+// 		//for ( auto it = wordLower.begin(); it != wordLower.end(); ++it )
+// 		//    *it |= 0x20;
+
+// 		TrieNode * tmp = root;
+// 		// for (char c : word) {
+// 		// 	tmp = tmp->next[getIndex(c | 0x20)];
+// 		// 	if (!tmp) {
+// 		// 		return false;
+// 		// 	}
+// 		// }
+// 		for (int i = 0; i < word.size(); ++i) {
+// 			tmp = tmp->next[getIndex(word[i] | 0x20)];	// "| 0x20" converts a word to lower case 
+// 			if (!tmp) {
+// 				return false;
+// 			}
+// 		}
+// 		return tmp->end;
+// 	}
+// 	inline void add(const std::string &word) {
+// 		std::string wordLower(word);
+// 		std::transform(word.begin(), word.end(), wordLower.begin(), ::tolower);
+// 		push(wordLower);
+// 	}
+// 	inline size_t size(void) const { return _size; }
+// 	SpellChecker_Trie() : root(new TrieNode()) {}
+// 	~SpellChecker_Trie() { delete root; }
+// private:
+// 	TrieNode * root;
+// 	size_t _size = 0;
+// };
 
 SpellChecker::SpellChecker(const enum ContainerType type)
 {
@@ -366,6 +473,6 @@ bool SpellChecker::is_valid(const std::string &word) {
 		return false;
 	for (int i = 1; i < word.size(); ++i)
 		if (!isalpha(word[i]) && word[i] != '\'')
-			return false;
+					return false;
 	return true;
 }
